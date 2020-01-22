2e74213cd6170740056d13d6ac469c1c16dede83
Ace240900 <ujjwalshrivastav3@gmail.com>
2020-01-22 14:40:41 +0530

    Modify php

--- CSE-B/phpInput.php
+++ CSE-B/phpInput.php
@@ -1,5 +1,11 @@
 <?php  
-echo 'Wecome'.' '.$_GET['username'];
+if (isset($_POST['btn'])){
+echo $_POST['id'];
+echo $_POST['name'];
+echo $_POST['age'];
+echo $_POST['contact'];
+echo $_POST['e-mail'];
+}
 ?>
 <html>
     <head>
@@ -8,7 +14,11 @@ echo 'Wecome'.' '.$_GET['username'];
     
     <body>
     <form action="phpInput.php" method="get">
-      Name: <input type="text" name="username"><br>
+        id: <input type="text" name="id"><br>
+	Name: <input type="text" name="Name"><br>
+	age: <input type="text" name="age"><br>
+	Contact: <input type="text" name="contact"><br>	
+	E-mail: <input type="text" name="email"><br>
       <input type="submit" name='btn'>
     </form>
     </body>
